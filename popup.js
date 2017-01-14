4a93889aadb94b99286ab8d4a06b2973f2b950c9
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-01-14 22:23:16 +0700

    updated scheleton...

--- popup.js
+++ popup.js
@@ -1,3 +1,28 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//app.js: declare routes
+(function(){
+
+    var app = angular.module("githubViewer", ["ngRoute"]);//inject ngRoute
+
+    app.config(function($routeProvider){
+        $routeProvider
+            .when("/main", {
+                templateUrl: "main.html",
+                controller: "MainController"
+            })
+            .when("/user/:username", {
+                templateUrl: "user.html",
+                controller: "UserController"
+            })
+            .when("/repo/:username/:reponame", {
+                templateUrl: "repo.html",
+                controller: "RepoController"
+            })
+            .otherwise({redirectTo:"/main"});
+    });
+
+}());
+
+//end here
+document.addEventListener('DOMContentLoaded', function () {
+  dumpBookmarks();
+});
